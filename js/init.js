8b17842512334c7ccd798e28eadd734faa709b55
jessakang <kang.jessicaa@gmail.com>
2016-07-17 18:45:04 +1000

    adding animations

--- js/init.js
+++ js/init.js
@@ -6,3 +6,27 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$("#about-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1500);
+});
+
+$("#projects-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 1500);
+});
+
+$("#resume-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#resume").offset().top
+    }, 1500);
+});
+
+$("#contact-nav").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contactme").offset().top
+    }, 1500);
+});
